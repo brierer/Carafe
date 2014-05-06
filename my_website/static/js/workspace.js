af9a3c8c9d8cc225553aabb0ab3fef5614a74df8
briere.r@gmail.com <briere.r@gmail.com>
2014-05-06 15:41:27 -0400

    Ajout de maybe + function

--- my_website/static/js/workspace.js
+++ my_website/static/js/workspace.js
@@ -57,7 +57,8 @@ function pollingServerCalcGetResult(nbTry) {
       var timesRun = nbTry;
       console.log('Action ' + (timesRun + 1) + ' started ' + (now - startTime) + 'ms after script start');
       if (data != null) {
-             displayData(data.eval);
+        $("#sdf").css('color', 'red');
+        displayData(data);
         isCalculatingWaiting = false;
       } else {
         setTimeout(function() {
@@ -136,89 +137,90 @@ function setIconTable() {
 function setDraggableWidget() {
   $(".ui-widget-content").draggable({
     containment: "parent",
-    handle: "thead" 
+    handle: "thead"
   });
 }
 
 
-function validateChartParameter(p){
-  if (p.title == undefined)  
+function validateChartParameter(p) {
+  if (p.title == undefined)
     (p.title = "")
   if (p.color == undefined)
     (p.color = "steelblue")
-      
-  return p  
+
+  return p
 }
 
 function displayChart(chart) {
   var p = validateChartParameter(chart.p);
 
-   $("#containment-wrapper").append("<div class='chart-container ui-widget-content draggable'><div class='chart-title'></div><div id='y_axis'></div><div id='chart' class='chart'></div><div id='x_axis'></div></div>")
-    var chartData = []
-    
+  $("#containment-wrapper").append("<div class='chart-container ui-widget-content draggable'><div class='chart-title'></div><div id='y_axis'></div><div id='chart' class='chart'></div><div id='x_axis'></div></div>")
+  var chartData = []
 
-    $(chart.y).each(function(i) {
-      chartData.push({
-        x: chart.x[i],
-        y: chart.y[i]
-      });
+
+  $(chart.y).each(function(i) {
+    chartData.push({
+      x: chart.x[i],
+      y: chart.y[i]
     });
+  });
 
-var format = function(n) {
-  return n.toFixed(2);
-}
+  var format = function(n) {
+    return n.toFixed(2);
+  }
 
-var graph = new Rickshaw.Graph( {
-    element: document.querySelector("#chart"),  
+  var graph = new Rickshaw.Graph({
+    element: document.querySelector("#chart"),
     renderer: 'lineplot',
     height: 250,
     width: 300,
     series: [{
-        color: p.color,
-        data: chartData,
-        name: 'y'
+      color: p.color,
+      data: chartData,
+      name: 'y'
     }]
-});
+  });
 
-var x_ticks = new Rickshaw.Graph.Axis.X( {
-  graph: graph,
-  grid: false,
-  orientation: 'bottom',
-  element: document.getElementById('x_axis'),
-  tickFormat: Rickshaw.Fixtures.Number.formatKMBT
-} );
+  var x_ticks = new Rickshaw.Graph.Axis.X({
+    graph: graph,
+    grid: false,
+    orientation: 'bottom',
+    element: document.getElementById('x_axis'),
+    tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+  });
 
-var y_ticks = new Rickshaw.Graph.Axis.Y( {
-  graph: graph,
-  grid: false,
-  scale: chart.y,
-  orientation: 'left',
-  tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-  element: document.getElementById('y_axis'),
-} );
+  var y_ticks = new Rickshaw.Graph.Axis.Y({
+    graph: graph,
+    grid: false,
+    scale: chart.y,
+    orientation: 'left',
+    tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+    element: document.getElementById('y_axis'),
+  });
 
 
-var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-  graph: graph,
-  formatter: function(series, x, y) {
-    var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + '"></span>';
-    var content = swatch + series.name + ": " + (y) + '<br>';
-    return content;
-  }
-} );
-graph.render();
-  $(".chart-title").html(p.title);   
+  var hoverDetail = new Rickshaw.Graph.HoverDetail({
+    graph: graph,
+    formatter: function(series, x, y) {
+      var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + '"></span>';
+      var content = swatch + series.name + ": " + (y) + '<br>';
+      return content;
+    }
+  });
+  graph.render();
+  $(".chart-title").html(p.title);
 }
 
 
 function displayData(data) {
-  if (data.statut == "ok")
-  {
-    displayResult(data.res);
-  }else if (data.statut == 'tko' ){
-    displayBadEval(data);
-  }else if (data.statut == 'ko'){
-    displayError(data);
+  editor.getDoc().setValue(decodeParse(data.parse));
+  editor.save();
+  if (data.eval.statut == "ok") {
+    displayResult(data.parse, data.eval.res);
+  } else if (data.statut == 'tko') {
+    displayBadEval(data.eval);
+  } else if (data.statut == 'ko') {
+    displayError(data.eval);
   }
 }
 
@@ -239,19 +241,19 @@ function displayBadEval(data) {
   $("#containment-wrapper").html(html);
 }
 
-function displayResult(data) {
+function displayResult(parse, data) {
   var html = "";
   $("#containment-wrapper").html(html);
   $(data).each(function(id, d) {
     if (this instanceof Array) {
       var table = validateTableWithArray(this);
-      displayOneTable(table, id);
+      displayOneTable(parse, table, id);
     }
     if (this.type == 'graph') {
-        displayChart(this);
+      displayChart(this);
     }
-    if (this.type == 'table') { 
-        displayOneTable(this, id);
+    if (this.type == 'table') {
+      displayOneTable(parse, this, id);
     }
   })
 
@@ -261,23 +263,28 @@ function displayResult(data) {
 
 }
 
-function validateTableWithArray(data){
-  var table = {data:data,p:{col:[]}}
+function validateTableWithArray(data) {
+  var table = {
+    data: data,
+    p: {
+      col: []
+    }
+  }
   $.each(table.data, function(i, col) {
     table.p.col = true;
   });
   return table;
-}  
+}
 
 
-function displayOneTable(table, id) {
-   $("#containment-wrapper").append("<div class='table-container ui-widget-content draggable'><div class='handsontable-wrapper'></div<</div>")  
+function displayOneTable(parse, table, id) {
+  $("#containment-wrapper").append("<div class='table-container ui-widget-content draggable'><div class='handsontable-wrapper'></div<</div>")
 
 
-  var widthTable = Math.min(75*table.data[0].length,750);
-  
+  var widthTable = Math.min(75 * table.data[0].length, 750);
+
   $('#containment-wrapper div').last().outerWidth(widthTable);
-  
+
   $('#containment-wrapper div.handsontable-wrapper').last().handsontable({
     data: table.data,
     colHeaders: table.p.col,
@@ -285,7 +292,14 @@ function displayOneTable(table, id) {
     contextMenu: true,
     stretchH: 'all',
     width: widthTable,
-    afterChange: function(hook) {if (hook!=null) changeTableValue(hook,id)}
+    cells: function(row, col, prop) {
+      var cellProperties = {};
+      cellProperties.readOnly = (isColReadOnly(col, parse, id))
+      return cellProperties;
+    },
+    afterChange: function(hook) {
+      if (hook != null) changeTableValue(hook, id)
+    }
   });
 
 };
@@ -301,7 +315,7 @@ function displayCells(col) {
 
 
 function displayOneCol(col) {
-  var html = '<th style="width: 20px;">'+col+'<i class="hidden fa fa-sort-asc pull-right" ></th>'
+  var html = '<th style="width: 20px;">' + col + '<i class="hidden fa fa-sort-asc pull-right" ></th>'
   return html
 };
 
@@ -336,142 +350,199 @@ $("a").dblclick(function() {
 $("#btn_eval").click(
   function() {
     eqEvaluation();
-   
   });
 
-String.prototype.splitKeep = function (splitter, ahead) {
-        var self = this;
-        var result = [];
-        // Substitution of matched string
-        function getSubst(value) {
-          var substChar = value[0] == '0' ? '1' : '0';
-          var subst = '';
-          for (var i = 0; i < value.length; i++) {
-              subst += substChar;
-          }
-          return subst;
-         };
-        if (splitter != '') {
-            var matches = [];
-            // Getting mached value and its index
-            var replaceName = splitter instanceof RegExp ? "replace" : "replaceAll";
-            var r = self[replaceName](splitter, function (m, i, e) {
-                matches.push({ value: m, index: i });
-                return getSubst(m);
-            });
-            // Finds split substrings
-            var lastIndex = 0;
-            for (var i = 0; i < matches.length; i++) {
-                var m = matches[i];
-                var nextIndex = ahead == true ? m.index : m.index + m.value.length;
-                if (nextIndex != lastIndex) {
-                    var part = self.substring(lastIndex, nextIndex);
-                    result.push(part);
-                    lastIndex = nextIndex;
-                }
-            };
-            if (lastIndex < self.length) {
-                var part = self.substring(lastIndex, self.length);
-                result.push(part);
-            };
-              
-        }
-        else {
-            result.add(self);
-        };
-        return result;
-    };
 
- changeTableValue(2,2);
-function changeTableValue(hook, id){
-  var text = $("#id_formulas").html()
-  var equations = splitEquations(text)
-  var code ={}
-  equations.forEach(function(value , i){
-    code[i] = convertEquation(value);
+
+function decodeParse(parse) {
+  var text = ""
+  parse.forEach(function(val, i) {
+    text += decodeEquation(val);
   })
-  alert(JSON.stringify(code[0]))
-  } 
+  //console.log(text);
+  return text
+}
+
+function decodeEquation(parse) {
+  return parse[0] + " = " + decodeVal(parse[1]);
+}
+
+function decodeVal(parse) {
+  var s1, v, s2 = ""
+  s1 = parse.s1
+  s2 = parse.s2
+  var value = parse.v
+  //console.log(JSON.stringify(value))
+  if (value.a !== undefined) {
+    v = decodeArray(value.a)
+  } else if (value.f !== undefined) {
+    v = decodeFunction(value.f)
+  } else if (value.o !== undefined) {
+    v = decodeObj(value.o)
+  } else {
+    v = decodeSingleValue(value.tag, value.contents);
+  }
+  return s1 + v + s2
+}
+
+function decodeArray(contents) {
+  var text = ""
+  contents.forEach(function(val, i) {
+    if (i != 0) {
+      text += ','
+    }
+    text += decodeVal(val);;
+  })
+
+  return "[" + text + "]";
+}
+
+
+function decodeFunction(f) {
+
+  if (f.arg.length == 0) {
+    return f.name;
+  } else {
+    var text = f.name + "("
+    var values = f.arg
+    values.forEach(function(val, i) {
+      if (i != 0) {
+        text += ','
+      }
+      text += decodeVal(val);;
+    })
+    return text + ")";
+  }
+}
+
+function decodeObj(contents) {
+  var text = ""
+  contents.forEach(function(content, i) {
+    if (i != 0) {
+      text += ','
+    }
+    tag = content[0];
+    value = decodeVal(content[1]);
+    text += tag + ':' + value
+  });
+  return "{" + text + "}";
+}
+
+function decodeSingleValue(tag, contents) {
+  if (tag == "Pstring") {
+    return "\"" + contents + "\""
+  } else {
+    return contents
+  }
+}
+
+
+function getFunction(name, i) {
+    
+  return function(parse) {
+    console.log(JSON.stringify(parse))
+    if (parse.v.f !== undefined) {
+      if (parse.v.f.name == name) {
+        return parse.v.f.arg[i]
+      } else {
+        return null
+      }
+    }
+  }
+  return null
+}
+
+function getArray(i) {
+  return function(parse) {
+  console.log(JSON.stringify(parse))
+    if (parse.v.a !== undefined) {
+      return parse.v.a[i]
+    }
+    return null
+  }
+}
+
+function isArray(parse) {
+    if (parse.v.a !== undefined){
+      return parse.v.a;
+    }
+    return null;    
+}
+
+function isColReadOnly(col, parse, id) {
+console.log(JSON.stringify(parse[0][1]))
+  m = Maybe(parse[0][1])
+  .bind(getFunction("show",0))
+  .bind(getArray(id))
+  .bind(getFunction("table",0))
+  .bind(getArray(col))
+  .bind(isArray)
   
+  return ((!m.isNothing()))
+  /* var arg = parse[0][1].v.f.arg[0]
 
+  var f = arg.v.a[id].v.f;
+  console.log(JSON.stringify(f))
+  if (f.name == 'table' && f.arg !== undefined && f.arg[0].v.a[0].v.a !== undefined) {
+    return false
+  }
+*/
+  //  return true
+  // }
+  //  if (isFunction(p.tag).tag=='FunctionValue'){
+  //  if (p.contents.contents[0]=='table' && p.contents.contents[1].tag=='ArrayValue'){
+  //   if (p.contents.contents[1].tag.contents.tag == 'ArrayValue'){
+  //     return true
+  //   }
+  //  }
+  // }
+  // if (p[col]!='ArrayValue' && p[col]!='FunctionValue'){
+  //  return true;
+  //}
 
-function splitEquations(text){
-  var patt = /\n+(?=[a-z][a-zA-Z]*\s*=)/gm;
-  return text.splitKeep(patt)
+  return false
 }
 
-function splitEquation(text){
- var patt = /\s?=\s?/gm;
- return text.splitKeep(patt)
-}
-
-function splitTag(text){
-  var tag = text.match(/[a-z][a-zA-Z]*(?=\s*=)/g)
-  return tag[0]
-}
 
 
+Maybe = function(value) {
+  var Nothing = {
+    bind: function(fn) {
+      return this;
+    },
+    isNothing: function() {
+      return true;
+    },
+    val: function() {
+      throw new Error("cannot call val() nothing");
+    },
+    maybe: function(def, fn) {
+      return def;
+    }
+  };
 
-function convertEquation(equation) {
-  var equationSplitted = splitEquation(equation)
-  var tagSplitted =  splitTag(equationSplitted[0]) 
-  var equation = {'tag':tagSplitted, 'value': convertValue(equationSplitted[1])}
-  return equation
-}
+  var Something = function(value) {
+    return {
+      bind: function(fn) {
+        return Maybe(fn.call(this, value));
+      },
+      isNothing: function() {
+        return false;
+      },
+      val: function() {
+        return value;
+      },
+      maybe: function(def, fn) {
+        return fn.call(this, value);
+      }
+    };
+  };
 
-function convertValue(text) {
-  if (isArray(text))    {return convertArray(text)} 
-  if (isFunction(text))  {return convertFunction(text)}
-  if (isNumber)  {return convertNumber(text)}
-  if (isString(text)) {return convertString(text)}
-  if (isBool(text)) {return convertBool(text)}  
-}
+  if (typeof value === 'undefined' ||
+    value === null ||
+    (typeof value.isNothing !== 'undefined' && value.isNothing())) {
+    return Nothing;
+  }
 
-function convertArray(text){
-  return {type:'Arr',val:text}
-
-}
-
-function convertSubValues(text){
-
-}
-
-function convertFunction(text){
-   var patt = /([a-z][a-zA-z]*|.*)/g
-   var splitFunction = text.match(patt);
-   return {type:'Func','name':splitFunction[0],'val':convertSubValues(splitFunction[1])}
-}
-function convertNumber(text){
-   return {type:'Arr',val:text}
-}
-function convertString(text){
-  return {type:'Str',val:text}
-}
-function convertBool(text){
-  return {type:'Bool',val:text}
-}
-
-function isBool(text){
-  var patt = /(true|false)/
-  return patt.test(text)
-}
-
-function isString(text){
- var patt = /".*/
-return patt.test(text)
-}
-
-function isNumber(text) {
-  var patt = /^-?\d*\.?\d*$/
-  return patt.test(text)
-}
-
-function isArray(text){
-  var patt = /^(\[).*/g
-  return patt.test(text)
-}
-
-function isFunction(text){
-  var patt = /[a-z][a-zA-Z]*\s?\(/g
-  return patt.test(text) 
-}
+  return Something(value);
+};
