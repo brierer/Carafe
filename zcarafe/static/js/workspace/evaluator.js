f00fd01052afb1b330dc95bebb1d976e38e27b0c
briere.r@gmail.com <briere.r@gmail.com>
2014-07-01 16:31:04 -0400

    Corretion getJSON

--- zcarafe/static/js/workspace/evaluator.js
+++ zcarafe/static/js/workspace/evaluator.js
@@ -26,13 +26,13 @@ function pollingServerCalcGetResult(nbTry) {
 	if (nbTry < 5) {
 		var id_form = $("#id_form_id").serialize();
 		var id_book = $("#id_book_id").serialize();
-		$.getJSON('getCalcResult/?' + id_form, function(r) {
+		$.get('getCalcResult/?' + id_form, function(data,status) {
 			var now = new Date().getTime();
 			var timesRun = nbTry;
 			console.log('Action ' + (timesRun + 1) + ' started ' + (now - startTime) + 'ms after script start');
-			if (r.data != null) {
-				eqWrapper.setEQ(r.data.parse)
-				displayData(r.data);
+			if (data != null) {
+ -				eqWrapper.setEQ(data.parse)
+ -				displayData(data);;
 				isCalculatingWaiting = false;
 			} else {
 				setTimeout(function() {
